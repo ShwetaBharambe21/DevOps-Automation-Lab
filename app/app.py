d8f8c28503df6acbbda129244b8b7cee362c5ff6
Shweta Bharambe <shwetabharambe2196@gmail.com>
2026-07-17 00:52:55 +0530

    Add Prometheus metrics

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest  
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter("request_count", "Total Request Count")
+
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
     return """
     <h1>🚀 DevOps Automation Lab</h1>
     <h3>Version 3</h3>
@@ -16,5 +20,9 @@ def health():
         "status": "healthy"
     }), 200
 
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
